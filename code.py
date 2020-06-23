5143dafb7cc40fee92fd57872b86f9e504670dc1
Kushagra Bansal <59137955+Kush1101@users.noreply.github.com>
2020-06-23 22:29:08 +0530

    Insert documentation

--- code.py
+++ code.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
 
 print(tf.__version__)
+
 dataset = tf.keras.datasets.fashion_mnist
 
 DESIRED_ACCURACY = 0.99
+
 class myCallBack(tf.model.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
         if logs.get('acc')>=DESIRED_ACCURACY:
@@ -11,14 +13,19 @@ class myCallBack(tf.model.callbacks.Callback):
             self.model.stop_training = True
             
 
-callbacks = myCallBack()
-(training_data,training_labels), (test_data,test_labels) = dataset.load()
+callbacks = myCallBack()    #instantiating the class myCallBack
+
+(training_data,training_labels), (test_data,test_labels) = dataset.load()   #loading the data from our data set
+
+#Reshape the data so it's in the form of a 4D list of dimensions 60000*28*28*1 (60000 images, width = 28, height= 28, colour info = 1(greyscale))
 training_data = training_data.reshape(60000,28,28,1)
 training_data = training_data/255.0
 
+#Reshape the test data
 test_data = test_data.reshape(10000,28,28,1)
 test_data = test_data/255.0
 
+#defining a model
 model = tf.keras.model.Sequential([
     tf.keras.layers.Conv2D(32, (3,3), activation = 'relu', input_shape = (28,28,1)),
     tf.keras.layers.MaxPooling2D(2,2),
@@ -32,7 +39,11 @@ model = tf.keras.model.Sequential([
     ])
     
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+#Summary provides us an overview of how the layers are manipulating the image and its parameters
 model.summary()
+#fitting the model with callbacks
 model.fit(training_data, training_labels, epochs = 15, callbacks = [callbacks])
+
 test_loss, test_acc = model.evaluate(test_data,test_labels)
 print("The accuracy of the model on test data is: " + str(test_acc))
